5880e5ebebadaa71148fc2ca3431acb5ee6301ec
miakojason <miakojason@gmail.com>
2023-12-29 15:34:29 +0800

    在登入程式中加上session,並根據登入狀況來顯示不同的內容

--- front/login.php
+++ front/login.php
@@ -10,8 +10,38 @@
             <td><input type="password" name="pw" id="pw"></td>
         </tr>
         <tr>
-            <td><input type="submit" value="登入"><input type="reset" value="清除"></td>
+            <td>
+                <input type="button" onclick="login()" value="登入">
+                <input type="reset" value="清除">
+            </td>
             <td><a href="?do=forget">忘記密碼</a> <a href="?do=reg">尚未註冊</a></td>
         </tr>
     </table>
 </fieldset>
+<script>
+    function login() {
+        $.post("./api/chk_acc.php", {
+            acc: $("#acc").val()
+        }, (res) => {
+            if (parseInt(res) == 0) {
+                alert("查無帳號")
+            } else {
+                $.post('./api/chk_pw.php', {
+                    acc: $("#acc").val(),
+                    pw: $("#pw").val()
+                }, (res) => {
+                    if (parseInt(res) == 1) {
+                        if ($("#acc").val() == 'admin') {
+                            location.href = './back.php'
+                        } else {
+                            location.href = './index.php'
+                        }
+                    } else {
+                        alert("密碼錯誤")
+                    }
+
+                })
+            }
+        })
+    }
+</script>
